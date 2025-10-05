eb3ca2d7072d19c366eccaabc5079afb07a7415d
Boluwatife <codedadebayo84@gmail.com>
2025-10-05 16:16:22 -0700

    worked on dasboard design

---
+++ app.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const Home = document.querySelector('.home');
+const Money = document.querySelector('.money');
+const Profile = document.querySelector('.profi');
+const DashBoard = document.querySelector('dashboard');
+

328364d721185793915faffb6fa63f52d736c749
Boluwatife <codedadebayo84@gmail.com>
2025-10-10 07:33:40 -0700

    new cards designes

--- Homepage/app.js
+++ Homepage/app.js
@@ -37,3 +37,57 @@ legal.addEventListener('mouseout', function(){
 
 //menu stay //
 
+
+
+
+//cards shadow effect //
+const card1 = document.querySelector('#card1');
+const card2 = document.querySelector('#card2');
+const card3 = document.querySelector('#card3');
+const card4 = document.querySelector('#card4');
+const card5 = document.querySelector('#card5');
+const card6 = document.querySelector('#card6');
+
+
+card1.addEventListener('mouseover', function(){
+    card1.classList.toggle('cards-shadow');
+})
+card1.addEventListener('mouseout', function(){
+    card1.classList.toggle('cards-shadow');
+})
+
+
+card2.addEventListener('mouseover', function(){
+    card2.classList.toggle('cards-shadow');
+})
+card2.addEventListener('mouseout', function(){
+    card2.classList.toggle('cards-shadow');
+})
+
+card3.addEventListener('mouseover', function(){
+    card3.classList.toggle('cards-shadow');
+})
+card3.addEventListener('mouseout', function(){
+    card3.classList.toggle('cards-shadow');
+})
+
+card4.addEventListener('mouseover', function(){
+    card4.classList.toggle('cards-shadow');
+})
+card4.addEventListener('mouseout', function(){
+    card4.classList.toggle('cards-shadow');
+})
+
+card5.addEventListener('mouseover', function(){
+    card5.classList.toggle('cards-shadow');
+})
+card5.addEventListener('mouseout', function(){
+    card5.classList.toggle('cards-shadow');
+})
+
+card6.addEventListener('mouseover', function(){
+    card6.classList.toggle('cards-shadow');
+})
+card6.addEventListener('mouseout', function(){
+    card6.classList.toggle('cards-shadow');
+})
